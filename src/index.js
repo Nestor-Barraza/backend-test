4ee9f8cd019228e42b1d4463a5696327ea890963
Nestor-Barraza <nestorbarrazaotalvarez@hotmail.com>
2023-06-25 18:15:32 -0500

    .

--- src/index.js
+++ src/index.js
@@ -1,34 +1,19 @@
 import DefaultTemplate from "utils/defaultTemplate";
 import app from "./server";
-import https from "https";
-import selfsigned from "selfsigned";
 import "colors";
 
 // Get port
 const PORT = app.get("port");
-
-// Generate a self-signed certificate and key
-const attrs = [{ name: "commonName", value: "localhost" }];
-const pems = selfsigned.generate(attrs, {
-  keySize: 2048,
-  algorithm: "sha256",
-});
-
-// Create options for the HTTPS server
-const options = {
-  key: pems.private,
-  cert: pems.cert,
-};
 // Server message
 app.use("/", (req, res) => {
   res.send(DefaultTemplate(PORT));
 });
 
-// Listen for incoming HTTPS requests
-https.createServer(options, app).listen(PORT, () => {
+// Start server
+app.listen(PORT, () => {
   try {
     console.log("Listen on port:".blue, `${PORT}`.rainbow.underline);
-   
+
   } catch (error) {
     console.log(error);
   }
--- src/utils/middlewares/https.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  if (!req.secure) {
-    return res.redirect(`https://${req.get("host")}${req.url}`);
-  }
-  next();
-};
